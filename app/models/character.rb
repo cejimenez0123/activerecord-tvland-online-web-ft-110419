73367de1fcbe2b3bb6a895e348ecc6e7da11dd5a
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-26 20:06:44 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,8 +4,5 @@ class Character < ActiveRecord::Base
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
-  def build_network
-    fox = Network.find_or_create_by(call_letters: "Fox")
-    self.network = fox
-  end
+
 end

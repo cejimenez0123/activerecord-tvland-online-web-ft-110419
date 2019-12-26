69319888fc694caa9819bfafa562e50cdfe7022f
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-26 19:48:06 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,7 +6,7 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
   def list_roles
-    self.characters.map(&:name)
+    "#{self.characters.map(&:name)} - #{self.shows.name}"
 
 
   end

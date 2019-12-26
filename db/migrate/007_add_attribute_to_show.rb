b43757ae51eaca2472ed925b150a0570f5bc6e8e
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-26 20:12:45 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_attribute_to_show.rb
+++ db/migrate/007_add_attribute_to_show.rb
@@ -1,6 +1,6 @@
 class AddAttributeToShow < ActiveRecord::Migration[5.1]
   def change
-    add_column :show, :day, :string
-    add_column :show, :season, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
